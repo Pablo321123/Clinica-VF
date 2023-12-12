1b4669807de66bdd3ab254589404fe5f331c32b6
Mateus <mribferraz2003@gmail.com>
2023-12-12 19:10:47 -0300

    Correções e adição de novas urls

--- trabalho/clinica/urls.py
+++ trabalho/clinica/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.entrarClinica, name = 'clinica')
+    path('entrar', views.entrarClinica, name = 'entrar'),
+    path('agendamento', views.agendamentoClinica, name = 'agendamento'),
+    path('', views.indexClinica, name = ''),
+    path('index', views.indexClinica, name = 'index'),
+    path('enderecos', views.enderecosClinica, name = 'enderecos'),
+    path('galeria', views.galeriaClinica, name = 'galeria'),
+    path('novoendereco', views.novoEnderecoClinica, name = 'novoEndereco'),
+    path('novofuncionario', views.novoFuncionarioClinica, name = 'novoFuncionario'),
+    path('novopaciente', views.novoPacienteClinica, name = 'novoPaciente'),
+    path('pacientes', views.pacientesClinica, name = 'pacientes')
 ]
--- trabalho/clinica/views.py
+++ trabalho/clinica/views.py
@@ -3,3 +3,27 @@ from django.http import HttpResponse
 
 def entrarClinica(request):
     return render(request, 'clinica/entrar.html')
+
+def agendamentoClinica(request):
+    return render(request, 'clinica/agendamento.html')
+    
+def indexClinica(request):
+    return render(request, 'clinica/index.html')
+    
+def enderecosClinica(request):
+    return render(request, 'clinica/enderecos.html')
+
+def galeriaClinica(request):
+    return render(request, 'clinica/galeria.html')
+
+def novoEnderecoClinica(request):
+    return render(request, 'clinica/novoendereco.html')
+
+def novoFuncionarioClinica(request):
+    return render(request, 'clinica/novofuncionario.html')
+
+def novoPacienteClinica(request):
+    return render(request, 'clinica/novopaciente.html')
+
+def pacientesClinica(request):
+    return render(request, 'clinica/pacientes.html')
