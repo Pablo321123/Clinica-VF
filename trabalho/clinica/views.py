c7befad83918f34404bfae5ff72f619cf590a670
Mateus <mribferraz2003@gmail.com>
2023-12-13 19:05:56 -0300

    Alterações autentição login

--- trabalho/clinica/views.py
+++ trabalho/clinica/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
+from django.conf import settings
+
+global logado
 
 def entrarClinica(request):
     if request.method == 'POST':    
@@ -16,6 +19,7 @@ def entrarClinica(request):
                 return HttpResponse('Email ou Senha incorreto!')
             funcionario = Funcionario.objects.get(codigo=pessoa)
             if funcionario.senha == senha:
+                settings.LOGADO = True
                 return redirect('/novofuncionario')
             else:
                 return HttpResponse('Email ou Senha incorreta!')
@@ -71,11 +75,15 @@ def agendamentoClinica(request):
         return render(request, 'clinica/agendamento.html', {'medicos':medicos})
     '''
 def indexClinica(request):
+    settings.LOGADO = False
     return render(request, 'clinica/index.html')
     
 def enderecosClinica(request):
-    enderecos = Endereco.objects.all().order_by('cep')
-    return render(request, 'clinica/enderecos.html', {'enderecos':enderecos})
+    if settings.LOGADO:
+        enderecos = Endereco.objects.all().order_by('cep')
+        return render(request, 'clinica/enderecos.html', {'enderecos':enderecos})
+    else:
+        return render(request, 'clinica/index.html')
 
 def galeriaClinica(request):
     return render(request, 'clinica/galeria.html')
@@ -95,34 +103,49 @@ def novoEnderecoClinica(request):
 
 
 def novoFuncionarioClinica(request):
-    return render(request, 'clinica/novofuncionario.html')
+    if settings.LOGADO:
+        return render(request, 'clinica/novofuncionario.html')
+    else:
+        return render(request, 'clinica/index.html')
 
 def novoPacienteClinica(request):
-    if request.method == 'POST':
-        form_pessoa = novaPessoaForm(request.POST)
-        form_paciente = PacienteForm(request.POST)
+    if settings.LOGADO:
+        if request.method == 'POST':
+            form_pessoa = novaPessoaForm(request.POST)
+            form_paciente = PacienteForm(request.POST)
 
-        if form_pessoa.is_valid() and form_paciente.is_valid():
-            pessoa = form_pessoa.save()
-            pessoas = Pessoa.objects.last()
-            paciente = form_paciente.save(commit=False)
-            paciente.codigo = pessoas
-            paciente.save()
-            return redirect('/index')
+            if form_pessoa.is_valid() and form_paciente.is_valid():
+                pessoa = form_pessoa.save()
+                pessoas = Pessoa.objects.last()
+                paciente = form_paciente.save(commit=False)
+                paciente.codigo = pessoas
+                paciente.save()
+                return redirect('/index')
 
+            else:
+                form_pessoa = novaPessoaForm(request.POST)
+                form_paciente = PacienteForm(request.POST)
+                return render(request, 'clinica/novopaciente.html', {'form_pessoa': form_pessoa, 'form_paciente': form_paciente})
     else:
-        form_pessoa = novaPessoaForm(request.POST)
-        form_paciente = PacienteForm(request.POST)
-        return render(request, 'clinica/novopaciente.html', {'form_pessoa': form_pessoa, 'form_paciente': form_paciente})
+        return render(request, 'clinica/index.html')
     
 def pacientesClinica(request):
-    pacientes = Paciente.objects.all().order_by('codigo')
-    return render(request, 'clinica/pacientes.html', {'pacientes': pacientes})
+    if settings.LOGADO:
+        pacientes = Paciente.objects.all().order_by('codigo')
+        return render(request, 'clinica/pacientes.html', {'pacientes': pacientes})
+    else:
+        return render(request, 'clinica/index.html')
 
 def todosAgendamentosClinica(request):
-    agendamentos = Agenda.objects.all().order_by('codigo')
-    return render(request, 'clinica/todosagendamentos.html', {'agendamentos':agendamentos})
+    if settings.LOGADO:
+        agendamentos = Agenda.objects.all().order_by('codigo')
+        return render(request, 'clinica/todosagendamentos.html', {'agendamentos':agendamentos})
+    else:
+        return render(request, 'clinica/index.html')
 
 def funcionariosClinica(request):
-    funcionarios = Funcionario.objects.all().order_by('codigo')
-    return render(request, 'clinica/funcionarios.html', {'funcionarios': funcionarios})
+    if settings.LOGADO:
+        funcionarios = Funcionario.objects.all().order_by('codigo')
+        return render(request, 'clinica/funcionarios.html', {'funcionarios': funcionarios})
+    else:
+        return render(request, 'clinica/index.html')
